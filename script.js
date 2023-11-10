ca349924000f4a6fe59292b8987250863cc1cb57
Doku-Desktop <joonkim2684@gmail.com>
2023-11-10 23:21:27 +0900

    Create playRound function

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-moves = ['rock', 'paper', 'scissors']
+let moves = ['Rock', 'Paper', 'Scissors'];
+let winAgainst = {
+    'Rock': 'Paper',
+    'Paper': 'Scissors',
+    'Scissors': 'Rock',
+};
 
 function getComputerChoice() {
     return moves[Math.floor(Math.random()*moves.length)]
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (winAgainst[playerSelection]==computerSelection) {
+        return "You Lose! " + computerSelection + " beats " + playerSelection
+    } 
+    else if (playerSelection == computerSelection) {
+        return "Tied! " + computerSelection + " ties against " + playerSelection
+    }
+    else {
+        return "You Win! " + playerSelection + " beats " + computerSelection
+    }
+}
